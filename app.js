cffd194af672ea4bcb3545df0ec9ff0e015e20c7
Ujjawl <srivastavujjawal123@gmail.com>
2020-11-03 10:30:57 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname,'/dist/index.htm'));
+});
+
+app.listen(process.env.PORT || 8080,
+    ()=>console.log("App Started"));
